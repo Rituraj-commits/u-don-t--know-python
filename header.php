3ac895bd8f51b80e6f434120dc8829d5cb5c97dc
Rituraj-commits <64738909+Rituraj-commits@users.noreply.github.com>
2020-05-03 20:00:50 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,43 @@
+<html>
+    <head>
+        <title>Birth Calculator </title>
+        <!-- Latest compiled and minified CSS -->
+        
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="newstyle.css"
+
+<meta name="viewport" content="device=device-width initial-scale=1.0">
+    </head>
+    <body>
+<!-- Header -->
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>                        
+                    </button>
+                   
+                    <a class="navbar-brand" href="index1.php"><span class="glyphicon glyphicon-btc"></span>irth<span class="glyphicon glyphicon-euro"></span>alculator</a>
+                  
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                       
+ 
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
