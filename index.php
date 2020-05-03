41326942711898ffb2f0d9d96540c42ced74d2ad
Rituraj-commits <64738909+Rituraj-commits@users.noreply.github.com>
2020-05-03 19:59:54 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<link rel="stylesheet" type="text/css" href="newstyle.css"
+
+<meta name="viewport" content="device=device-width initial-scale=1.0">
+
+        <title>Birth Calculator</title>
+    <body style="padding-top: 50px;">
+        <?php
+        require 'header.php';
+        ?>
+         
+         <div id="content">
+            <!--Main banner image-->
+            <div id = "banner_image">
+                <div class="container">	
+                    <center>
+                        <div id="banner_content">
+                            <h4>"So much universe and so little time"</h4>
+                            <p>Let's calculate how much time has been elapsed since your birth</p>
+                            <br/>
+                            <a  href="user.php" class="btn btn-primary btn-lg active">Find out</a>
+                        </div>
+                    </center>
+                </div>
+            </div>
+            <!--Main banner image end-->
+               <!--Footer-->
+        <?php
+        require 'footer1.php';
+        ?>
+    </body>
+        
+    </head>
+</html>
